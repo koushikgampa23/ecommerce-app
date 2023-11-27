18d15baa54d65dd7fba7b9315e7c6b6b42127f8d
koushikgampa <koushikg@asiapac.altair.com>
2023-11-27 22:52:49 +0530

    Added a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ItemListing } from "./item-listing/ItemListing";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { ProductListing } from "./product-listing/ProductListing";
 import { Checkout } from "./checkout/Checkout";
+import classes from "./App.module.css";
 
 export const AppContext = React.createContext({});
 
@@ -11,10 +12,16 @@ function App() {
   return (
     <Router>
       <AppContext.Provider value={{ cartItems, setCartItems }}>
-        <Link to="/itemlisting" >item Listing</Link>
-        <Link to="/checkout">Checkout</Link>
+        <div className={classes.navBar}>
+          <Link to="/" className={classes.link}>
+            Products
+          </Link>
+          <Link to="/checkout" className={classes.link}>
+            Checkout
+          </Link>
+        </div>
         <Routes>
-          <Route path="/itemlisting" element={<ItemListing />} />
+          <Route path="/" element={<ItemListing />} />
           <Route path="/productlisting/:id" element={<ProductListing />} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
